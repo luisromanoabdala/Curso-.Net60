14cc22f14a48a9cfe504cac22c8086c7b6db98d9
luisromanoabdala <luisromanoabdala@gmail.com>
2022-05-18 18:45:15 -0300

    Agregar archivos de proyecto.

---
+++ Tarea 5/Tarea 5/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.WriteLine("Ingrese su nombre:");
+string nombre;
+nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+Console.Write("Desea continuar? (S/N)");
+var texto = Console.ReadLine();
+
+while (texto.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Hola, ingrese su nombre:");
+
+    nombre = Console.ReadLine();
+    Console.WriteLine("Hola " + nombre);
+
+    Console.WriteLine("Desea continuar? (S/N)");
+    texto = Console.ReadLine();
+}
+
+if (texto.ToUpper() == "N")
+{
+
+    Console.WriteLine("Programa finalizado correctamente");
+
+}
+
+else
+{
+    Console.WriteLine("Opcion no valida");
+}
