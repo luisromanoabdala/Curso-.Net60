08e71a4b573c82c624448bd770299817c9d4fb2a
luisromanoabdala <luisromanoabdala@gmail.com>
2022-05-25 17:57:25 -0300

    se creo la Tarea 6

---
+++ tarea6/Tarea6/Tarea6/Program.cs
@@ -0,0 +1,22 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+int intentos = 0;
+int numeroIngresado;
+do
+{
+    intentos++;
+    Console.WriteLine("Ingrese un numero: ");
+    numeroIngresado = int.Parse(Console.ReadLine());
+    if (numeroIngresado > numeroSecreto)
+    {
+        Console.WriteLine("El nÃºmero ingresado es muy grande. Intenta de nuevo.");
+    }
+    else if (numeroIngresado < numeroSecreto)
+    {
+        Console.WriteLine("El nÃºmero ingresado es muy chico. Intenta de nuevo.");
+    }
+    else
+    {
+        Console.WriteLine($"Felicitaciones, has adivinado el nÃºmero secreto que era:  {numeroSecreto}");
+        Console.WriteLine($"Lo has logrado en {intentos} intentos!!");
+    }
+} while (numeroSecreto != numeroIngresado);
---
+++ tarea7/Tarea7/Tarea 7/Tarea 7/Program.cs
@@ -0,0 +1,43 @@
+﻿int suma = 0;
+int max=0,  min= 0;
+double promedio = 0;
+int[] numeros = new int[10];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el numero {i+1} ");
+    numeros[i] = int.Parse(Console.ReadLine());
+    if (i == 0)
+    {
+        max = numeros[i];
+        min = numeros[i];
+    }
+    else {
+        if (numeros[i] > max)
+        {
+            max = numeros[i];
+        }
+        if(numeros[i] < min)
+        {
+            min = numeros[i];
+        }
+     }
+    suma += numeros[i];
+    
+}
+
+promedio = suma / 10;
+Console.WriteLine("Los datos son: ");
+for(int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"El numero {i+1} es : {numeros[i]} ");
+
+}
+
+Console.WriteLine($" La suma de los numeros es : {suma}");
+Console.WriteLine($" El promedio de los numeros es : {promedio}");
+Console.WriteLine($" El maximo  de los numeros es : {max}");
+Console.WriteLine($" El minimo de los numeros es : {min}");
+
+
+
